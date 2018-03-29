cc307404f2bd301496ab71e680ed56902872a3c1
Mikhail.udot <Mikhail.udot@venafi.com>
2018-03-29 19:57:00 +0300

    Initial Commit

---
+++ PodPlayerSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PodPlayerSwift
+//
+//  Created by Mikhail Udot on 29.03.2018.
+//  Copyright © 2018 Mikhail Udot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
