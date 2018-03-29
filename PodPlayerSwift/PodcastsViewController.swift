3cf59bd2b8cade027b6309c7708b897da61c4c39
Mikhail.udot <Mikhail.udot@venafi.com>
2018-03-29 20:59:56 +0300

    PodcastViewController added

---
+++ PodPlayerSwift/PodcastsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PodcastsViewController.swift
+//  PodPlayerSwift
+//
+//  Created by Mikhail Udot on 29.03.2018.
+//  Copyright © 2018 Mikhail Udot. All rights reserved.
+//
+
+import Cocoa
+
+class PodcastsViewController: NSViewController {
+
+    @IBOutlet weak var PodcastTextField: NSTextField!
+    @IBOutlet weak var AddPodcastButton: NSButton!
+
+    @IBAction func AddPodcastButtonClick(_ sender: NSButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
