a9c23e5b7a67f69962283a4c649cdb508af340d9
Mikhail.udot <Mikhail.udot@venafi.com>
2018-03-31 22:11:12 +0300

    Controls definitions added to controller

--- PodPlayerSwift/EpisodesViewController.swift
+++ PodPlayerSwift/EpisodesViewController.swift
@@ -10,6 +10,15 @@ import Cocoa
 
 class EpisodesViewController: NSViewController {
 
+    @IBOutlet weak var TitleLabel: NSTextField!
+    @IBOutlet weak var ImageView: NSImageView!
+    @IBOutlet weak var PausePlayButton: NSButton!
+    @IBOutlet weak var DeleteButton: NSButton!
+    @IBAction func PlayPauseButtonClick(_ sender: NSButton) {
+    }
+    @IBAction func DeleteButtonClick(_ sender: NSButton) {
+    }
+    @IBOutlet weak var TableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
