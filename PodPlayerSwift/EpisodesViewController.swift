c8477f1d9d877a2ac7b8e07b9788f10e08f82f03
Mikhail.udot <Mikhail.udot@venafi.com>
2018-03-31 22:03:37 +0300

    EpisodesViewController

---
+++ PodPlayerSwift/EpisodesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodesViewController.swift
+//  PodPlayerSwift
+//
+//  Created by Mikhail Udot on 31.03.2018.
+//  Copyright © 2018 Mikhail Udot. All rights reserved.
+//
+
+import Cocoa
+
+class EpisodesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
