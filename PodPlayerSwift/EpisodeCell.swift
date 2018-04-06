1bf95e666d910a760a10fc5adceef609b5ef40b5
Mikhail.udot <Mikhail.udot@venafi.com>
2018-04-06 18:45:39 +0300

    EpisodeCell cocoa class with controls added

---
+++ PodPlayerSwift/EpisodeCell.swift
@@ -0,0 +1,23 @@
+//
+//  EpisodeCell.swift
+//  PodPlayerSwift
+//
+//  Created by Mikhail Udot on 06.04.2018.
+//  Copyright © 2018 Mikhail Udot. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class EpisodeCell: NSTableCellView {
+
+    @IBOutlet weak var TitleLabel: NSTextField!
+    @IBOutlet weak var OubdateLabel: NSTextField!
+    @IBOutlet weak var DescriptionWebView: WKWebView!
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
